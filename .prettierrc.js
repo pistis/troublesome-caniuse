755054a1b4bcc456feb921535806648e72c62073
pistis <vamalboro@gmail.com>
2019-06-13 18:27:56 +0900

    add .prettierrc config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 120,
+}
