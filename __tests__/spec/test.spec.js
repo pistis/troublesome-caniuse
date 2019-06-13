ea7422ee766ee3bfb9018853478919ba51e828ef
pistis <vamalboro@gmail.com>
2019-06-13 18:47:15 +0900

    configure test env by jest

---
+++ __tests__/spec/test.spec.js
@@ -0,0 +1,8 @@
+describe('test for test', () => {
+  test('number 1 should be number type.', () => {
+    expect(typeof 1).toBe('number')
+  })
+  test('string "str" should be string type.', () => {
+    expect(typeof 'str').toBe('string')
+  })
+})
